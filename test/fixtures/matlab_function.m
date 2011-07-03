97145bbfc8b75633b6a0f30c3a78ca07fda54131
bilderbuchi <bilderbuchi@phononoia.at>
2011-07-03 12:44:32 +0200

    Add simple test fixtures for Matlab.

---
+++ test/fixtures/matlab_function.m
@@ -0,0 +1,9 @@
+function ret = matlab_function(A,B)
+% Simple function adding two values and displaying the return value
+
+ret = A+B;
+% Display the return value
+disp('Return value in function');
+disp(ret);
+
+
