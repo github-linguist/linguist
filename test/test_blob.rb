da515105975f2df3d1fc2c5897e6da85771d5f13
Ted Nyman <ted@ted.io>
2013-03-06 15:28:55 -0800

    Nix this generated check for now

--- test/test_blob.rb
+++ test/test_blob.rb
@@ -189,10 +189,6 @@ class TestBlob < Test::Unit::TestCase
 
     assert blob("JavaScript/intro.js").generated?
     assert blob("JavaScript/classes.js").generated?
-
-    # Cython-generated C/C++
-    assert blob("C/sgd_fast.c").generated?
-    assert blob("C++/wrapper_inner.cpp").generated?
   end
 
   def test_vendored
