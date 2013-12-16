a49303c93fd07d02e186f0b3aa4b2d4777d8b485
Arfon Smith <arfon@github.com>
2013-12-16 15:27:48 -0800

    Expanding TestHeuristcs to include all Objective C header files

--- test/test_heuristics.rb
+++ test/test_heuristics.rb
@@ -14,11 +14,18 @@ class TestHeuristcs < Test::Unit::TestCase
   def fixture(name)
     File.read(File.join(samples_path, name))
   end
+  
+  # Only calling out '.h' filenames as these are the ones causing issues
+  def all_h_fixtures(language_name)
+    Dir.glob("#{samples_path}/#{language_name}/*.h")
+  end
 
-  def test_find_by_heuristics
+  def test_obj_c_by_heuristics
     languages = ["C++", "Objective-C"]
-    results = Heuristics.find_by_heuristics(fixture("Objective-C/StyleViewController.h"), languages)
-    assert_equal Language["Objective-C"], results.first
+    all_h_fixtures("Objective-C").each do |fixture|
+      results = Heuristics.find_by_heuristics(fixture("Objective-C/#{File.basename(fixture)}"), languages)
+      assert_equal Language["Objective-C"], results.first
+    end
   end
 
   def test_detect_still_works_if_nothing_matches
