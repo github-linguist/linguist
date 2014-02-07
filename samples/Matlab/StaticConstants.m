4bbbe2fb475b96ae3a69d6aff209bd950b37d868
Christopher Granade <cgranade@cgranade.com>
2014-02-07 12:30:17 -0500

    Added new test case for MATLAB.
    This test case is based on misdetection of
    https://github.com/CoryGroup/quantum-utils-matlab/blob/master/src/SIConstants.m

---
+++ samples/Matlab/StaticConstants.m
@@ -0,0 +1,12 @@
+classdef StaticConstants
+    % StaticConstants - Sample of a MATLAB class with no instance properties or
+    %     methods. Has an H1 line and a few useless static and constant
+    %     properties.
+
+    properties (Constant)
+        foo = 'bar'
+        a = 'b'
+        c = 3.0
+    end
+
+end
