e209d2cca605ea5e3c38bf3c52a9a90a597de0c8
cristaloleg <cristaloleg@outlook.com>
2014-02-18 20:19:52 +0200

    add *.hh sample for C/C++ $953

---
+++ samples/C++/config.hh
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
