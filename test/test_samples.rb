2239b0e7eea058427e7fef131c90ed89af09155b
Brandon Keepers <brandon@opensoul.org>
2014-04-24 11:28:35 -0400

    test case to ensure samples are not binary

--- test/test_samples.rb
+++ test/test_samples.rb
@@ -35,15 +35,15 @@ class TestSamples < Test::Unit::TestCase
     assert_equal data['tokens_total'], data['language_tokens'].inject(0) { |n, (_, c)| n += c }
     assert_equal data['tokens_total'], data['tokens'].inject(0) { |n, (_, ts)| n += ts.inject(0) { |m, (_, c)| m += c } }
   end
-  
+
   # If a language extension isn't globally unique then make sure there are samples
   def test_presence
     Linguist::Language.all.each do |language|
       language.all_extensions.each do |extension|
         language_matches = Language.find_by_filename("foo#{extension}")
-        
+
         # If there is more than one language match for a given extension
-        # then check that there are examples for that language with the extension 
+        # then check that there are examples for that language with the extension
         if language_matches.length > 1
           language_matches.each do |language|
             assert File.directory?("samples/#{language.name}"), "#{language.name} is missing a samples directory"
@@ -53,4 +53,24 @@ class TestSamples < Test::Unit::TestCase
       end
     end
   end
+
+  Samples.each do |sample|
+    define_method "test_#{sample[:path]}_not_binary" do
+      blob = blob(sample[:path])
+      assert !blob.binary_mime_type? && !blob.binary?, "#{sample[:path]} is a binary file"
+    end
+  end
+
+  # FIXME: extract all of these to `test/helper.rb`
+  require 'linguist/file_blob'
+  require 'linguist/samples'
+  def samples_path
+    File.expand_path("../../samples", __FILE__)
+  end
+
+  def blob(name)
+    name = File.join(samples_path, name) unless name =~ /^\//
+    FileBlob.new(name, samples_path)
+  end
+
 end
