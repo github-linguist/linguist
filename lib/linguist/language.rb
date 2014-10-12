e5b60017596aeb4246b8c558d1ecb288a79d92c7
Vicent Marti <tanoku@gmail.com>
2014-10-12 17:43:03 +0200

    Add support for TextMate scopes

--- lib/linguist/language.rb
+++ lib/linguist/language.rb
@@ -290,6 +290,16 @@ module Linguist
       @lexer = Pygments::Lexer.find_by_name(attributes[:lexer] || name) ||
         raise(ArgumentError, "#{@name} is missing lexer")
 
+      @tm_scope = attributes[:tm_scope] || begin
+        context = case @type
+                  when :data, :markup, :prose
+                    'text'
+                  when :programming, nil
+                    'source'
+                  end
+        "#{context}.#{@name.downcase}"
+      end
+
       @ace_mode = attributes[:ace_mode]
       @wrap = attributes[:wrap] || false
 
@@ -362,6 +372,11 @@ module Linguist
     #
     # Returns the Lexer
     attr_reader :lexer
+
+    # Public: Get the name of a TextMate-compatible scope
+    #
+    # Returns the scope
+    attr_reader :tm_scope
 
     # Public: Get Ace mode
     #
