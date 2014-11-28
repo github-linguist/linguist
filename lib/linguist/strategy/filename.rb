a53b6825d33dd635e73729a22d19dcf5be11236d
Brandon Keepers <brandon@opensoul.org>
2014-11-28 16:49:40 -0600

    Remove .script! hack

--- lib/linguist/strategy/filename.rb
+++ lib/linguist/strategy/filename.rb
@@ -3,17 +3,7 @@ module Linguist
     # Detects language based on filename and/or extension
     class Filename
       def self.call(blob, _)
-        name = blob.name.to_s
-
-        # A bit of an elegant hack. If the file is executable but extensionless,
-        # append a "magic" extension so it can be classified with other
-        # languages that have shebang scripts.
-        extensions = FileBlob.new(name).extensions
-        if extensions.empty? && blob.mode && (blob.mode.to_i(8) & 05) == 05
-          name += ".script!"
-        end
-
-        Language.find_by_filename(name)
+        Language.find_by_filename(blob.name.to_s)
       end
     end
   end
