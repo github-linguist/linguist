ad778571a24101aaccb60e5807a65a84970269af
Garen Torikian <gjtorikian@gmail.com>
2014-12-05 16:57:55 +0200

    This `reject` is no longer necessary

--- lib/linguist/language.rb
+++ lib/linguist/language.rb
@@ -271,7 +271,7 @@ module Linguist
     #
     # Returns an Array of Languages.
     def self.ace_modes
-      @ace_modes ||= all.select(&:ace_mode).sort_by { |lang| lang.name.downcase }.reject { |h| h.ace_mode == 'none' }
+      @ace_modes ||= all.select(&:ace_mode).sort_by { |lang| lang.name.downcase }
     end
 
     # Internal: Initialize a new Language
