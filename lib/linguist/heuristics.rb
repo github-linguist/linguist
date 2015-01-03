6f4557a103f097951ea69e02a1deb05ca2e3ba5d
Arfon Smith <arfon@github.com>
2015-01-03 11:10:24 -0600

    Updating Objective-C matchers and adding a sample C++ file

--- lib/linguist/heuristics.rb
+++ lib/linguist/heuristics.rb
@@ -70,7 +70,7 @@ module Linguist
     end
 
     disambiguate "Objective-C", "C++", "C" do |data|
-      if (/@(interface|class|protocol|property|end|synchronised|selector|implementation)\b/.match(data))
+      if (/^[ \t]*@(interface|class|protocol|property|end|synchronised|selector|implementation)\b/.match(data))
         Language["Objective-C"]
       elsif (/^\s*#\s*include <(cstdint|string|vector|map|list|array|bitset|queue|stack|forward_list|unordered_map|unordered_set|(i|o|io)stream)>/.match(data) ||
         /^\s*template\s*</.match(data) || /^[ \t]*try/.match(data) || /^[ \t]*catch\s*\(/.match(data) || /^[ \t]*(class|(using[ \t]+)?namespace)\s+\w+/.match(data) || /^[ \t]*(private|public|protected):$/.match(data) || /std::\w+/.match(data))
