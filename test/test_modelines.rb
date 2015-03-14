fda0f2a0423e7211ae64fd4d980167a687a5311e
michael tesch <tesch1@gmail.com>
2015-03-14 23:53:17 +0100

    detect emacs modeline for fundamental as Text

--- test/test_modelines.rb
+++ test/test_modelines.rb
@@ -19,6 +19,7 @@ class TestModelines < Minitest::Test
     assert_modeline Language["C++"], fixture_blob("Data/Modelines/seeplusplusEmacs7")
     assert_modeline Language["C++"], fixture_blob("Data/Modelines/seeplusplusEmacs8")
     assert_modeline Language["C++"], fixture_blob("Data/Modelines/seeplusplusEmacs9")
+    assert_modeline Language["Text"], fixture_blob("Data/Modelines/fundamentalEmacs.c")
     assert_modeline Language["Prolog"], fixture_blob("Data/Modelines/not_perl.pl")
     assert_modeline Language["Smalltalk"], fixture_blob("Data/Modelines/example_smalltalk.md")
     assert_modeline Language["PHP"], fixture_blob("Data/Modelines/iamphp.inc")
@@ -36,6 +37,7 @@ class TestModelines < Minitest::Test
     assert_equal Language["C++"], fixture_blob("Data/Modelines/seeplusplusEmacs7").language
     assert_equal Language["C++"], fixture_blob("Data/Modelines/seeplusplusEmacs8").language
     assert_equal Language["C++"], fixture_blob("Data/Modelines/seeplusplusEmacs9").language
+    assert_equal Language["Text"], fixture_blob("Data/Modelines/fundamentalEmacs.c").language
     assert_equal Language["Prolog"], fixture_blob("Data/Modelines/not_perl.pl").language
     assert_equal Language["Smalltalk"], fixture_blob("Data/Modelines/example_smalltalk.md").language
     assert_equal Language["PHP"], fixture_blob("Data/Modelines/iamphp.inc").language
