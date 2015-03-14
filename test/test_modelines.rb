a364e4a2dcf71eb5f5d972a9b2c57d67f5c63da0
michael tesch <tesch1@gmail.com>
2015-03-14 23:13:59 +0100

    tests for emacs modeline regex

--- test/test_modelines.rb
+++ test/test_modelines.rb
@@ -10,6 +10,15 @@ class TestModelines < Minitest::Test
   def test_modeline_strategy
     assert_modeline Language["Ruby"], fixture_blob("Data/Modelines/ruby")
     assert_modeline Language["C++"], fixture_blob("Data/Modelines/seeplusplus")
+    assert_modeline Language["C++"], fixture_blob("Data/Modelines/seeplusplusEmacs1")
+    assert_modeline Language["C++"], fixture_blob("Data/Modelines/seeplusplusEmacs2")
+    assert_modeline Language["C++"], fixture_blob("Data/Modelines/seeplusplusEmacs3")
+    assert_modeline Language["C++"], fixture_blob("Data/Modelines/seeplusplusEmacs4")
+    assert_modeline Language["C++"], fixture_blob("Data/Modelines/seeplusplusEmacs5")
+    assert_modeline Language["C++"], fixture_blob("Data/Modelines/seeplusplusEmacs6")
+    assert_modeline Language["C++"], fixture_blob("Data/Modelines/seeplusplusEmacs7")
+    assert_modeline Language["C++"], fixture_blob("Data/Modelines/seeplusplusEmacs8")
+    assert_modeline Language["C++"], fixture_blob("Data/Modelines/seeplusplusEmacs9")
     assert_modeline Language["Prolog"], fixture_blob("Data/Modelines/not_perl.pl")
     assert_modeline Language["Smalltalk"], fixture_blob("Data/Modelines/example_smalltalk.md")
     assert_modeline Language["PHP"], fixture_blob("Data/Modelines/iamphp.inc")
@@ -18,6 +27,15 @@ class TestModelines < Minitest::Test
   def test_modeline_languages
     assert_equal Language["Ruby"], fixture_blob("Data/Modelines/ruby").language
     assert_equal Language["C++"], fixture_blob("Data/Modelines/seeplusplus").language
+    assert_equal Language["C++"], fixture_blob("Data/Modelines/seeplusplusEmacs1").language
+    assert_equal Language["C++"], fixture_blob("Data/Modelines/seeplusplusEmacs2").language
+    assert_equal Language["C++"], fixture_blob("Data/Modelines/seeplusplusEmacs3").language
+    assert_equal Language["C++"], fixture_blob("Data/Modelines/seeplusplusEmacs4").language
+    assert_equal Language["C++"], fixture_blob("Data/Modelines/seeplusplusEmacs5").language
+    assert_equal Language["C++"], fixture_blob("Data/Modelines/seeplusplusEmacs6").language
+    assert_equal Language["C++"], fixture_blob("Data/Modelines/seeplusplusEmacs7").language
+    assert_equal Language["C++"], fixture_blob("Data/Modelines/seeplusplusEmacs8").language
+    assert_equal Language["C++"], fixture_blob("Data/Modelines/seeplusplusEmacs9").language
     assert_equal Language["Prolog"], fixture_blob("Data/Modelines/not_perl.pl").language
     assert_equal Language["Smalltalk"], fixture_blob("Data/Modelines/example_smalltalk.md").language
     assert_equal Language["PHP"], fixture_blob("Data/Modelines/iamphp.inc").language
