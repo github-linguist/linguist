2a0b0e9f934573d5f61cedb6445202f242038bfa
Paul Chaignon <paul.chaignon@gmail.com>
2016-03-12 11:17:29 +0100

    Fix heuristic rule for PLpgSQL

--- lib/linguist/heuristics.rb
+++ lib/linguist/heuristics.rb
@@ -354,7 +354,7 @@ module Linguist
     end
 
     disambiguate ".sql" do |data|
-      if /^\\i\b|AS \$\$|LANGUAGE '+plpgsql'+/i.match(data) || /SECURITY (DEFINER|INVOKER)/i.match(data) || /BEGIN( WORK| TRANSACTION)?;/i.match(data)
+      if /^\\i\b|AS \$\$|LANGUAGE '?plpgsql'?/i.match(data) || /SECURITY (DEFINER|INVOKER)/i.match(data) || /BEGIN( WORK| TRANSACTION)?;/i.match(data)
         #Postgres
         Language["PLpgSQL"]
       elsif /(alter module)|(language sql)|(begin( NOT)+ atomic)/i.match(data)  || /signal SQLSTATE '[0-9]+'/i.match(data)
