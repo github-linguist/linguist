47fb367ae67907b7f08e79ea89363feb5d37df68
Lars Brinkhoff <lars@nocrew.org>
2016-09-28 12:55:56 +0200

    Add '</' to Markdown heuristic.
    Also consider empty files as Markdown.

--- lib/linguist/heuristics.rb
+++ lib/linguist/heuristics.rb
@@ -254,7 +254,7 @@ module Linguist
     end
 
     disambiguate ".md" do |data|
-      if /^[-a-z0-9=#!\*\[|]/i.match(data)
+      if /(^[-a-z0-9=#!\*\[|])|<\//i.match(data) || data.empty?
         Language["Markdown"]
       elsif /^(;;|\(define_)/.match(data)
         Language["GCC machine description"]
