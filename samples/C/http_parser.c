6f4b6ade626f3c6a734f639f8ac95f2194308781
Josh Soref <jsoref@users.noreply.github.com>
2017-11-12 05:35:37 +0000

    spelling: automatically

--- samples/C/http_parser.c
+++ samples/C/http_parser.c
@@ -1671,7 +1671,7 @@ size_t http_parser_execute (http_parser *parser,
             && parser->content_length != ULLONG_MAX);
 
         /* The difference between advancing content_length and p is because
-         * the latter will automaticaly advance on the next loop iteration.
+         * the latter will automatically advance on the next loop iteration.
          * Further, if content_length ends up at 0, we want to see the last
          * byte again for our message complete callback.
          */
