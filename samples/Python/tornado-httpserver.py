147a0197e1122591cd18669789aa99abaeb869cb
Josh Soref <jsoref@users.noreply.github.com>
2017-11-12 06:45:25 +0000

    spelling: connection

--- samples/Python/tornado-httpserver.py
+++ samples/Python/tornado-httpserver.py
@@ -155,7 +155,7 @@ class HTTPConnection(object):
     """Handles a connection to an HTTP client, executing HTTP requests.
 
     We parse HTTP headers and bodies, and execute the request callback
-    until the HTTP conection is closed.
+    until the HTTP connection is closed.
     """
     def __init__(self, stream, address, request_callback, no_keep_alive=False,
                  xheaders=False):
