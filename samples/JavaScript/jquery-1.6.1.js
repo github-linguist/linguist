da39e3bb73c8a636e25fda05dac669a3dbfabea2
Josh Soref <jsoref@users.noreply.github.com>
2017-11-27 05:17:09 +0000

    spelling: multifunctional

--- samples/JavaScript/jquery-1.6.1.js
+++ samples/JavaScript/jquery-1.6.1.js
@@ -798,7 +798,7 @@ jQuery.extend({
 		return proxy;
 	},
 
-	// Mutifunctional method to get and set values to a collection
+	// Multifunctional method to get and set values to a collection
 	// The value/s can be optionally by executed if its a function
 	access: function( elems, key, value, exec, fn, pass ) {
 		var length = elems.length;
