cd573de7c46d282addcbc22d1e975aa76db60e89
Vicent Marti <tanoku@gmail.com>
2018-01-29 13:29:31 +0100

    compiler: Prefer grammar extensions based on type

--- tools/grammars/compiler/loader_fs.go
+++ tools/grammars/compiler/loader_fs.go
@@ -6,6 +6,7 @@ import (
 	"os/exec"
 	"path"
 	"path/filepath"
+	"sort"
 	"strings"
 )
 
@@ -14,14 +15,43 @@ type fsLoader struct {
 	abspath string
 }
 
+var preferredGrammars = map[string]int{
+	".tmlanguage":      0,
+	".cson":            1,
+	".json":            1,
+	".plist":           2,
+	".yaml-tmlanguage": 3,
+}
+
+func findPreferredExtension(ext []string) string {
+	if len(ext) > 1 {
+		sort.Slice(ext, func(i, j int) bool {
+			a := strings.ToLower(ext[i])
+			b := strings.ToLower(ext[j])
+			return preferredGrammars[a] < preferredGrammars[b]
+		})
+	}
+	return ext[0]
+}
+
 func (l *fsLoader) findGrammars() (files []string, err error) {
+	grammars := make(map[string][]string)
+
 	err = filepath.Walk(l.abspath,
 		func(path string, info os.FileInfo, err error) error {
 			if err == nil && isValidGrammar(path, info) {
-				files = append(files, path)
+				ext := filepath.Ext(path)
+				base := path[0 : len(path)-len(ext)]
+				grammars[base] = append(grammars[base], ext)
 			}
 			return nil
 		})
+
+	for base, ext := range grammars {
+		pref := findPreferredExtension(ext)
+		files = append(files, base+pref)
+	}
+
 	return
 }
 
