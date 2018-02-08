90c784d4b6f8f5037059a569e391b6bf89a8482a
Joshua Weinstein <jweinstein@splunk.com>
2018-02-08 10:25:02 -0800

    Changed C Regex to detect void functions, restrict keyword, and flexible array parameters

--- lib/linguist/heuristics.rb
+++ lib/linguist/heuristics.rb
@@ -75,7 +75,11 @@ module Linguist
     end
 
     # Common heuristics
-    CRegex = /^\s*#\s*include <(stdlib|stdio|stdarg|stdbool|stdint|stddef|setjmp|errno)\.h>/
+    CRegex = Regexp.union(
+        /^(char|double|short|int|long|float|unsigned|void)*\w\s*\w\s*\(\s*void\s*\)/,
+        /^(\w|\s|\*)*restrict/,
+        /^(char|short|int|long|float|unsigned)*\s+\w+\s*\[\]\s*;/
+      )
     CPlusPlusRegex = Regexp.union(
         /^\s*#\s*include <(cstdint|string|vector|map|list|array|bitset|queue|stack|forward_list|unordered_map|unordered_set|(i|o|io)stream)>/,
         /^\s*template\s*</,
