d1cefb6199054aea2887b2fedeff1c31060764ce
Rob Quist <rob.quist@dukesoft.nl>
2018-07-02 17:47:17 +0200

    Add heuristics testcase for game maker languages

--- test/test_heuristics.rb
+++ test/test_heuristics.rb
@@ -131,6 +131,13 @@ class TestHeuristics < Minitest::Test
     })
   end
 
+  def test_gml_by_heuristics
+    assert_heuristics({
+      "Game Maker Language" => all_fixtures("Game Maker Language", "*.gml"),
+      "Graph Modeling Language" => all_fixtures("Graph Modeling Language", "*.gml")
+    })
+  end
+
   def test_fs_by_heuristics
     assert_heuristics({
       "F#" => all_fixtures("F#", "*.fs"),
