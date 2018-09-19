972052639a95d95276bc7ab25a68880a6652b263
Colin Seymour <colin@github.com>
2018-09-19 14:43:47 +0100

    Store grammar git version within license file when caching (#4268)
    * Add version when caching licenses
    
    * Add approved grammar licenses to config.yml
    
    * Add version and update cache of all licenses
    
    All licenses should remain the same except where there is clearly a change in license
    
    * Update release process with license caching deets
    
    * Double check licensed cached before checking status
    
    * Only need to explicitly approve grammars without license files
    
    * Revert "Double check licensed cached before checking status"
    
    This reverts commit c86cfd1ed5ce68f65885e9309cc592f8a41bce07.
    
    This isn't needed now we're versioning out license files
    
    * Change into submodule dir before getting SHA
    
    * Update Licensed min version requirement
    
    * Update license formats from Licensed 1.3.3 processing
    
    * Remove tests covered by "licensed status" test
    
    * 🔥 more redundant tests and associated methods
    
    * Update hy.tmLanguage cached license

--- test/test_grammars.rb
+++ test/test_grammars.rb
@@ -3,53 +3,6 @@ require_relative "./helper"
 class TestGrammars < Minitest::Test
   ROOT = File.expand_path("../..", __FILE__)
 
-  # List of projects that are allowed without licenses
-  PROJECT_WHITELIST = [
-    "vendor/grammars/Sublime-Lasso",      # No license file
-    "vendor/grammars/blitzmax",           # No license file
-    "vendor/grammars/creole",             # License filename is not LICENSE(.*)?
-  ].freeze
-
-  HASH_WHITELIST = [
-    "4b4c1b4aff11b99e53091f95b24093655c0d9ed2", # Agda.tmbundle
-    "a92c0e4e644206078a158d663b9f033d0d0aefbf", # ant.tmbundle
-    "98eac91395c97ae090113e1b4527cae3228d563a", # bro-sublime
-    "220e011c8d686129e9c4163a7c655b9d64f61e59", # elixir-tmbundle
-    "55f431fb78e7b3cb6d18ef1042f70ebf4b8d838d", # factor
-    "b81acf2ba52d312754bf5055845a723123bda388", # FreeMarker.tmbundle
-    "ee77ce4cf9121bccc3e37ba6b98f8e7acd589aaf", # gap-tmbundle
-    "4cfc7ce12de920ccc836bbab2d748151d5ba7e38", # go-tmbundle
-    "01868f4a2476fad9abb8b7199ea1547d33cd48db", # hy.tmLanguage
-    "6c2e34d62c08f97a3e2ece3eedc65fbd99873ff4", # idl.tmbundle
-    "e68efca5a844aa78729cadcf42507013151e6605", # jflex.tmbundle
-    "39f092c726491ca6a02354dbc6c3a0920bb44d4c", # mako-tmbundle
-    "7821982b18bc35d6925cc16ece68d9c71f1fbba3", # moonscript-tmbundle
-    "806dae0a87f95409496223dfd9a45cc57c878aeb", # PHP-Twig.tmbundle
-    "0c216b112f3a4e6d5848128504d8378d8c7eee00", # r.tmbundle
-    "da39a3ee5e6b4b0d3255bfef95601890afd80709", # SCSS.tmbundle
-    "68539730d3cde34355f429f2267e265c1e030912", # smalltalk-tmbundle
-    "4b5f67a54532ca6e49ba44cd135a510a74712e07", # Stylus
-    "23d2538e33ce62d58abda2c039364b92f64ea6bc", # sublime-angelscript
-    "38ce879dc8b217a080caa2ff9e2241c9505256d6", # sublime-shen
-    "3df4ef028c6384b64bc59b8861d6c52093b2116d", # sublime-text-ox
-    "c18bcd787325167bf1405629259c091d49064541", # Std license in README.md of many TextMate grammars like abap.tmbundle
-  ].freeze
-
-  # List of allowed SPDX license names
-  LICENSE_WHITELIST = %w[
-    apache-2.0
-    bsd-2-clause
-    bsd-3-clause
-    isc
-    mit
-    mpl-2.0
-    public
-    textmate
-    unlicense
-    wtfpl
-    zlib
-  ].freeze
-
   def setup
     @grammars = YAML.load(File.read(File.join(ROOT, "grammars.yml")))
   end
@@ -89,59 +42,6 @@ class TestGrammars < Minitest::Test
     assert_equal current_data, updated_data, "Grammar list is out-of-date. Run `script/list-grammars`"
   end
 
-  def test_submodules_have_recognized_licenses
-    unrecognized = submodule_licenses.select { |k,v| v.nil? && Licensee.project(k).license_file }
-    unrecognized.reject! { |k,v| PROJECT_WHITELIST.include?(k) }
-    message = "The following submodules have unrecognized licenses:\n* #{unrecognized.keys.join("\n* ")}\n"
-    message << "Please ensure that the project's LICENSE file contains the full text of the license"
-    assert_equal Hash.new, unrecognized, message
-  end
-
-  def test_submodules_have_licenses
-    unlicensed = submodule_licenses.select { |k,v| v.nil? }.reject { |k,v| PROJECT_WHITELIST.include?(k) }
-    message = "The following submodules don't have licenses:\n* #{unlicensed.keys.join("\n* ")}\n"
-    message << "Please ensure that the project has a LICENSE file, and that the LICENSE file contains the full text of the license"
-    assert_equal Hash.new, unlicensed, message
-  end
-
-  def test_submodules_have_approved_licenses
-    unapproved = submodule_licenses.reject { |k,v| LICENSE_WHITELIST.include?(v) ||
-                                                   PROJECT_WHITELIST.include?(k) ||
-                                                   HASH_WHITELIST.include?(v) }
-                                   .map { |k,v| "#{k}: #{v}"}
-    message = "The following submodules have unapproved licenses:\n* #{unapproved.join("\n* ")}\n"
-    message << "The license must be added to the LICENSE_WHITELIST in /test/test_grammars.rb once approved"
-    assert_equal [], unapproved, message
-  end
-
-  def test_whitelisted_submodules_dont_have_licenses
-    licensed = submodule_licenses.reject { |k,v| v.nil? }.select { |k,v| PROJECT_WHITELIST.include?(k) }
-    message = "The following whitelisted submodules have a license:\n* #{licensed.keys.join("\n* ")}\n"
-    message << "Please remove them from the project whitelist"
-    assert_equal Hash.new, licensed, message
-  end
-
-  def test_whitelisted_hashes_dont_have_licenses
-    used_hashes = submodule_licenses.values.reject { |v| v.nil? || LICENSE_WHITELIST.include?(v) }
-    unused_hashes = HASH_WHITELIST - used_hashes
-    message = "The following whitelisted license hashes are unused:\n* #{unused_hashes.join("\n* ")}\n"
-    message << "Please remove them from the hash whitelist"
-    assert_equal Array.new, unused_hashes, message
-  end
-
-  def test_submodules_whitelist_has_no_extra_entries
-    skip("Need to work out how to handle dual-licensed entities")
-    extra_whitelist_entries = PROJECT_WHITELIST - submodule_licenses.select { |k,v| v.nil? }.keys
-    not_present = extra_whitelist_entries.reject { |k,v| Dir.exist?(k) }
-    licensed = extra_whitelist_entries.select { |k,v| submodule_licenses[k] }
-
-    msg = "The following whitelisted submodules don't appear to be part of the project:\n* #{not_present.join("\n* ")}"
-    assert_equal [], not_present, msg
-
-    msg = "The following whitelisted submodules actually have licenses and don't need to be whitelisted:\n* #{licensed.join("\n* ")}"
-    assert_equal [], licensed, msg
-  end
-
   def test_submodules_use_https_links
     File.open(".gitmodules", "r") do |fh|
       ssh_submodules = []
@@ -162,35 +62,4 @@ class TestGrammars < Minitest::Test
     @submodule_paths ||= `git config --list --file "#{File.join(ROOT, ".gitmodules")}"`.lines.grep(/\.path=/).map { |line| line.chomp.split("=", 2).last }.reject { |path| path =~ /CodeMirror/ }
   end
 
-  # Returns a hash of submodules in the form of submodule_path => license
-  def submodule_licenses
-    @@submodule_licenses ||= begin
-      submodules = {}
-      submodule_paths.each { |submodule| submodules[submodule] = submodule_license(submodule) }
-      submodules
-    end
-  end
-
-  # Given the path to a submodule, return its SPDX-compliant license key
-  # If the license is unrecognized, return its hash
-  def submodule_license(submodule)
-    # Prefer Licensee to detect a submodule's license
-    project = Licensee.project(submodule, detect_packages: true, detect_readme: true)
-    return project.license.key if project.licenses.length == 1 && !project.license.pseudo_license?
-
-    # If we have more than one license, return the first one that isn't a
-    # pseudo-license (other or no-license), if any
-    if project.licenses.length > 1
-      first_real_license = project.licenses.reject{ |f| f.pseudo_license? }.first
-      return first_real_license.key unless first_real_license.nil?
-    end
-
-    # We know a license exists, but no method was able to recognize it.
-    # We return the license hash in this case, to uniquely identify it.
-    if project.license_file
-      return project.license_file.content_hash
-    elsif project.readme
-      return project.readme.content_hash
-    end
-  end
 end
