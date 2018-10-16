1c7a523c65121eb136f52efd81b0a50a5be0c74a
Rob Quist <RobQuistNL@users.noreply.github.com>
2018-10-16 13:24:47 +0200

    Update generated.rb

--- lib/linguist/generated.rb
+++ lib/linguist/generated.rb
@@ -589,7 +589,7 @@ module Linguist
       return false unless ['.yy', '.yyp'].include? extname
       return false unless lines.count > 3
       return lines[2].match(/\"modelName\"\:\s*\"GM/) ||
-             lines[0].start_with?("1.0.0")
+             lines.first.match(/\d\.\d\.\d/\u2190.*/)
     end
   end
 end
