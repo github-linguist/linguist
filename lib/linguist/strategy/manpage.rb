24d3e96621f0bbb3eb786be77db94454e47816b6
Alhadis <gardnerjohng@gmail.com>
2018-11-04 22:28:28 +1100

    Extend recognition logic for ManpageStrategy

--- lib/linguist/strategy/manpage.rb
+++ lib/linguist/strategy/manpage.rb
@@ -10,10 +10,10 @@ module Linguist
 
       # RegExp for matching man(7) and mdoc(7) title macros 
       TITLE_MACRO = /
-        ^[.']     [ \t]*
-        (?:TH|Dt) [ \t]+
-        (?<name> "[^\n"]+"|[^ \s"]+) [ \t]+
-        (?<sect> "[^\n"]+"|[^ \s"]+)
+        ^ [.']
+        [ \t]* (?<macro> TH|Dt)
+        [ \t]+ (?<name>  "[^\n"]+"|[^ \s"]+)
+        [ \t]+ (?<sect>  "[^\n"]+"|[^ \s"]+)
       /x
 
       # Public: Detect a Roff manpage based on its content and file extension.
@@ -31,21 +31,47 @@ module Linguist
       # Wrapper method which handles the actual checking routines.
       def self.valid_manpage?(blob)
         if MANPAGE_EXTS.match?(blob.name)
-          lines = blob.first_lines(SEARCH_SCOPE).join("\n")
-
-          if match = TITLE_MACRO.match(lines)
-            file_name = blob.name.downcase.gsub(/\.[^.]+$/, "")
-            file_sect = blob.extension.downcase.gsub(/^\./, "")
-            param_name = match[:name].downcase.gsub(/^"|"$/, "")
-            param_sect = match[:sect].downcase.gsub(/^"|"$/, "")
-
-            # Be lenient towards autoconf variables like "@NAME@"
-            autoconf_name = /^@.+@$/.match?(param_name)
-            autoconf_sect = /^@.+@$/.match?(param_sect)
-        
-            return (autoconf_name || file_name == param_name) &&
-                   (autoconf_sect || file_sect == param_sect)
+          header = blob.first_lines(SEARCH_SCOPE).join("\n")
+          if match = TITLE_MACRO.match(header)
+            title_matches_filename?(match, blob) ||
+            is_header_conventional?(match, header)
           end
+        end
+      end
+
+      # Internal: First test we make, which should match most manpages.
+      #
+      # match - A #<MatchData> object returned from the extension match
+      # blob  - An object which quacks like a blob
+      #
+      # Returns a Boolean.
+      def self.title_matches_filename?(match, blob)
+        file_name  = blob.name.downcase.gsub(/\.[^.]+$/, "")
+        file_sect  = blob.extension.downcase.gsub(/^\./, "")
+        param_name = match[:name].downcase.gsub(/^"|"$/, "")
+        param_sect = match[:sect].downcase.gsub(/^"|"$/, "")
+
+        # NB: The /^@.+@$/ matches are for autoconf variables
+        (file_name == param_name || /^@.+@$/.match?(param_name)) &&
+        (file_sect == param_sect || /^@.+@$/.match?(param_sect))
+      end
+
+      # Internal: Second test that's run if the first happened to fail. 
+      #
+      # match - A #<MatchData> object returned from the extension match
+      # blob  - An object which quacks like a blob
+      #
+      # Returns a Boolean.
+      def self.is_header_conventional?(match, header)
+        if "Dt" == match[:macro]
+          # mdoc: Well-formed prologues will contain the following:
+          /^[.'][ \t]*Dd[ \t]+\S/.match?(header) && # Document date
+          /^[.'][ \t]*Os(?=\s|$)/.match?(header) && # System identifier
+          /^[.'][ \t]*Sh[ \t]+(NAME\b|"NAME")/.match?(header) # A "NAME" section
+        else
+          # man: Few expectations apply, so just match two common sections:
+          /^[.'][ \t]*SH[ \t]+(NAME\b|"NAME")/.match?(header) &&      # "NAME"
+          /^[.'][ \t]*SH[ \t]+(SYNOPSIS\b|"SYNOPSIS")/.match?(header) # "SYNOPSIS"
         end
       end
     end
