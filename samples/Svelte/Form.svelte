1868988303889109f05028b5010538ad52552d77
Umang Galaiya <umang.galaiya@razorpay.com>
2018-11-12 16:55:45 +0530

    Add support for Svelte.

---
+++ samples/Svelte/Form.svelte
@@ -0,0 +1,37 @@
+<form on:submit="createHandler(event)">
+  <div>
+    <label for="slinky-title">Title</label>
+    <input
+      bind:value='title'
+      id="slinky-title"
+      name="title"
+      type="text"
+    />
+  </div>
+  <button type="submit">Add Now</button>
+  <button type="button" on:click="logState()">Log</button>
+</form>
+
+<script>
+  const defaultData = {
+    title: '',
+  }
+
+  export default {
+    data: () => defaultData,
+
+    methods: {
+      logState() {
+        console.log(this.get())
+      },
+
+      createHandler(event) {
+        event.preventDefault()
+        // parse, validate, fire, reset
+        const slinky = this.get()
+        this.fire('create', slinky)
+        this.set(defaultData)
+      }
+    },
+  }
+</script>
