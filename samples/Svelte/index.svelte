a65f068760cbe87819752d411b214e7f881fa0b0
Umang Galaiya <umang.galaiya@razorpay.com>
2019-04-29 01:01:35 +0530

    Update Svelte sample

---
+++ samples/Svelte/index.svelte
@@ -0,0 +1,26 @@
+<script>
+  import SomeComponent from './somecomonent.svelte';
+
+  export let name = '';
+
+  $: console.log('Your name is', name);
+</script>
+
+<input type="text" placeholder="Name" bind:value={name}>
+
+{#if name}
+  <h1>Hello, {name}!</h1>
+{:else}
+  <h3>Please enter your name.</h3>
+{/if}
+
+<style>
+  h1 {
+    font-weight: 600;
+    color: #0a0a0a;
+  }
+
+  h3 {
+    color: gray;
+  }
+</style>
