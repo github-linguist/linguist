83d95b6863952783afd70ca1fecc85ccd7d41a37
John Fultz <jfultz@wolfram.com>
2020-07-02 14:12:16 -0500

    Fix tests for "Mathematica"->Wolfram Language" change.
    * Fix sorting
    * Fix name of samples directory with Wolfram Language samples
    * Add .wls sample file

--- samples/Mathematica/Problem12.m
+++
@@ -1,8 +0,0 @@
-(* ::Package:: *)
-
-(* Problem12.m *)
-(* Author: William Woodruff *)
-(* Problem: What is the value of the first triangle number to have over five hundred divisors? *)
-
-Do[If[Length[Divisors[Binomial[i + 1, 2]]] > 500, 
-  Print[Binomial[i + 1, 2]]; Break[]], {i, 1000000}]
