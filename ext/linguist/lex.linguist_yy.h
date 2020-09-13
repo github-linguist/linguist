dfe947a0a6b7b649f55a68adae6d590b412dbb15
Santiago M. Mola <santi@mola.io>
2020-09-13 19:56:31 +0200

    Update tokenizer to flex 2.6.4
    Re-generate tokenizer with flex 2.6.4 (current version on Ubuntu 20.04
    and Homebrew).

--- ext/linguist/lex.linguist_yy.h
+++ ext/linguist/lex.linguist_yy.h
@@ -10,10 +10,220 @@
 
 #define FLEX_SCANNER
 #define YY_FLEX_MAJOR_VERSION 2
-#define YY_FLEX_MINOR_VERSION 5
-#define YY_FLEX_SUBMINOR_VERSION 35
+#define YY_FLEX_MINOR_VERSION 6
+#define YY_FLEX_SUBMINOR_VERSION 4
 #if YY_FLEX_SUBMINOR_VERSION > 0
 #define FLEX_BETA
+#endif
+
+#ifdef yy_create_buffer
+#define linguist_yy_create_buffer_ALREADY_DEFINED
+#else
+#define yy_create_buffer linguist_yy_create_buffer
+#endif
+
+#ifdef yy_delete_buffer
+#define linguist_yy_delete_buffer_ALREADY_DEFINED
+#else
+#define yy_delete_buffer linguist_yy_delete_buffer
+#endif
+
+#ifdef yy_scan_buffer
+#define linguist_yy_scan_buffer_ALREADY_DEFINED
+#else
+#define yy_scan_buffer linguist_yy_scan_buffer
+#endif
+
+#ifdef yy_scan_string
+#define linguist_yy_scan_string_ALREADY_DEFINED
+#else
+#define yy_scan_string linguist_yy_scan_string
+#endif
+
+#ifdef yy_scan_bytes
+#define linguist_yy_scan_bytes_ALREADY_DEFINED
+#else
+#define yy_scan_bytes linguist_yy_scan_bytes
+#endif
+
+#ifdef yy_init_buffer
+#define linguist_yy_init_buffer_ALREADY_DEFINED
+#else
+#define yy_init_buffer linguist_yy_init_buffer
+#endif
+
+#ifdef yy_flush_buffer
+#define linguist_yy_flush_buffer_ALREADY_DEFINED
+#else
+#define yy_flush_buffer linguist_yy_flush_buffer
+#endif
+
+#ifdef yy_load_buffer_state
+#define linguist_yy_load_buffer_state_ALREADY_DEFINED
+#else
+#define yy_load_buffer_state linguist_yy_load_buffer_state
+#endif
+
+#ifdef yy_switch_to_buffer
+#define linguist_yy_switch_to_buffer_ALREADY_DEFINED
+#else
+#define yy_switch_to_buffer linguist_yy_switch_to_buffer
+#endif
+
+#ifdef yypush_buffer_state
+#define linguist_yypush_buffer_state_ALREADY_DEFINED
+#else
+#define yypush_buffer_state linguist_yypush_buffer_state
+#endif
+
+#ifdef yypop_buffer_state
+#define linguist_yypop_buffer_state_ALREADY_DEFINED
+#else
+#define yypop_buffer_state linguist_yypop_buffer_state
+#endif
+
+#ifdef yyensure_buffer_stack
+#define linguist_yyensure_buffer_stack_ALREADY_DEFINED
+#else
+#define yyensure_buffer_stack linguist_yyensure_buffer_stack
+#endif
+
+#ifdef yylex
+#define linguist_yylex_ALREADY_DEFINED
+#else
+#define yylex linguist_yylex
+#endif
+
+#ifdef yyrestart
+#define linguist_yyrestart_ALREADY_DEFINED
+#else
+#define yyrestart linguist_yyrestart
+#endif
+
+#ifdef yylex_init
+#define linguist_yylex_init_ALREADY_DEFINED
+#else
+#define yylex_init linguist_yylex_init
+#endif
+
+#ifdef yylex_init_extra
+#define linguist_yylex_init_extra_ALREADY_DEFINED
+#else
+#define yylex_init_extra linguist_yylex_init_extra
+#endif
+
+#ifdef yylex_destroy
+#define linguist_yylex_destroy_ALREADY_DEFINED
+#else
+#define yylex_destroy linguist_yylex_destroy
+#endif
+
+#ifdef yyget_debug
+#define linguist_yyget_debug_ALREADY_DEFINED
+#else
+#define yyget_debug linguist_yyget_debug
+#endif
+
+#ifdef yyset_debug
+#define linguist_yyset_debug_ALREADY_DEFINED
+#else
+#define yyset_debug linguist_yyset_debug
+#endif
+
+#ifdef yyget_extra
+#define linguist_yyget_extra_ALREADY_DEFINED
+#else
+#define yyget_extra linguist_yyget_extra
+#endif
+
+#ifdef yyset_extra
+#define linguist_yyset_extra_ALREADY_DEFINED
+#else
+#define yyset_extra linguist_yyset_extra
+#endif
+
+#ifdef yyget_in
+#define linguist_yyget_in_ALREADY_DEFINED
+#else
+#define yyget_in linguist_yyget_in
+#endif
+
+#ifdef yyset_in
+#define linguist_yyset_in_ALREADY_DEFINED
+#else
+#define yyset_in linguist_yyset_in
+#endif
+
+#ifdef yyget_out
+#define linguist_yyget_out_ALREADY_DEFINED
+#else
+#define yyget_out linguist_yyget_out
+#endif
+
+#ifdef yyset_out
+#define linguist_yyset_out_ALREADY_DEFINED
+#else
+#define yyset_out linguist_yyset_out
+#endif
+
+#ifdef yyget_leng
+#define linguist_yyget_leng_ALREADY_DEFINED
+#else
+#define yyget_leng linguist_yyget_leng
+#endif
+
+#ifdef yyget_text
+#define linguist_yyget_text_ALREADY_DEFINED
+#else
+#define yyget_text linguist_yyget_text
+#endif
+
+#ifdef yyget_lineno
+#define linguist_yyget_lineno_ALREADY_DEFINED
+#else
+#define yyget_lineno linguist_yyget_lineno
+#endif
+
+#ifdef yyset_lineno
+#define linguist_yyset_lineno_ALREADY_DEFINED
+#else
+#define yyset_lineno linguist_yyset_lineno
+#endif
+
+#ifdef yyget_column
+#define linguist_yyget_column_ALREADY_DEFINED
+#else
+#define yyget_column linguist_yyget_column
+#endif
+
+#ifdef yyset_column
+#define linguist_yyset_column_ALREADY_DEFINED
+#else
+#define yyset_column linguist_yyset_column
+#endif
+
+#ifdef yywrap
+#define linguist_yywrap_ALREADY_DEFINED
+#else
+#define yywrap linguist_yywrap
+#endif
+
+#ifdef yyalloc
+#define linguist_yyalloc_ALREADY_DEFINED
+#else
+#define yyalloc linguist_yyalloc
+#endif
+
+#ifdef yyrealloc
+#define linguist_yyrealloc_ALREADY_DEFINED
+#else
+#define yyrealloc linguist_yyrealloc
+#endif
+
+#ifdef yyfree
+#define linguist_yyfree_ALREADY_DEFINED
+#else
+#define yyfree linguist_yyfree
 #endif
 
 /* First, we deal with  platform-specific or compiler-specific issues. */
@@ -49,7 +259,6 @@ typedef int16_t flex_int16_t;
 typedef uint16_t flex_uint16_t;
 typedef int32_t flex_int32_t;
 typedef uint32_t flex_uint32_t;
-typedef uint64_t flex_uint64_t;
 #else
 typedef signed char flex_int8_t;
 typedef short int flex_int16_t;
@@ -57,7 +266,6 @@ typedef int flex_int32_t;
 typedef unsigned char flex_uint8_t; 
 typedef unsigned short int flex_uint16_t;
 typedef unsigned int flex_uint32_t;
-#endif /* ! C99 */
 
 /* Limits of integral types. */
 #ifndef INT8_MIN
@@ -88,27 +296,23 @@ typedef unsigned int flex_uint32_t;
 #define UINT32_MAX             (4294967295U)
 #endif
 
+#ifndef SIZE_MAX
+#define SIZE_MAX               (~(size_t)0)
+#endif
+
+#endif /* ! C99 */
+
 #endif /* ! FLEXINT_H */
 
-#ifdef __cplusplus
+/* begin standard C++ headers. */
 
-/* The "const" storage-class-modifier is valid. */
-#define YY_USE_CONST
-
-#else	/* ! __cplusplus */
-
-/* C99 requires __STDC__ to be defined as 1. */
-#if defined (__STDC__)
-
-#define YY_USE_CONST
-
-#endif	/* defined (__STDC__) */
-#endif	/* ! __cplusplus */
-
-#ifdef YY_USE_CONST
+/* TODO: this is always defined, so inline it */
 #define yyconst const
+
+#if defined(__GNUC__) && __GNUC__ >= 3
+#define yynoreturn __attribute__((__noreturn__))
 #else
-#define yyconst
+#define yynoreturn
 #endif
 
 /* An opaque pointer. */
@@ -130,7 +334,15 @@ typedef void* yyscan_t;
 
 /* Size of default input buffer. */
 #ifndef YY_BUF_SIZE
+#ifdef __ia64__
+/* On IA-64, the buffer size is 16k, not 8k.
+ * Moreover, YY_BUF_SIZE is 2*YY_READ_BUF_SIZE in the general case.
+ * Ditto for the __ia64__ case accordingly.
+ */
+#define YY_BUF_SIZE 32768
+#else
 #define YY_BUF_SIZE 16384
+#endif /* __ia64__ */
 #endif
 
 #ifndef YY_TYPEDEF_YY_BUFFER_STATE
@@ -155,12 +367,12 @@ struct yy_buffer_state
 	/* Size of input buffer in bytes, not including room for EOB
 	 * characters.
 	 */
-	yy_size_t yy_buf_size;
+	int yy_buf_size;
 
 	/* Number of characters read into yy_ch_buf, not including EOB
 	 * characters.
 	 */
-	yy_size_t yy_n_chars;
+	int yy_n_chars;
 
 	/* Whether we "own" the buffer - i.e., we know we created it,
 	 * and can realloc() it to grow it, and should free() it to
@@ -183,7 +395,7 @@ struct yy_buffer_state
 
     int yy_bs_lineno; /**< The line count. */
     int yy_bs_column; /**< The column count. */
-    
+
 	/* Whether to try to fill the input buffer when we reach the
 	 * end of it.
 	 */
@@ -194,21 +406,21 @@ struct yy_buffer_state
 	};
 #endif /* !YY_STRUCT_YY_BUFFER_STATE */
 
-void linguist_yyrestart (FILE *input_file ,yyscan_t yyscanner );
-void linguist_yy_switch_to_buffer (YY_BUFFER_STATE new_buffer ,yyscan_t yyscanner );
-YY_BUFFER_STATE linguist_yy_create_buffer (FILE *file,int size ,yyscan_t yyscanner );
-void linguist_yy_delete_buffer (YY_BUFFER_STATE b ,yyscan_t yyscanner );
-void linguist_yy_flush_buffer (YY_BUFFER_STATE b ,yyscan_t yyscanner );
-void linguist_yypush_buffer_state (YY_BUFFER_STATE new_buffer ,yyscan_t yyscanner );
-void linguist_yypop_buffer_state (yyscan_t yyscanner );
+void yyrestart ( FILE *input_file , yyscan_t yyscanner );
+void yy_switch_to_buffer ( YY_BUFFER_STATE new_buffer , yyscan_t yyscanner );
+YY_BUFFER_STATE yy_create_buffer ( FILE *file, int size , yyscan_t yyscanner );
+void yy_delete_buffer ( YY_BUFFER_STATE b , yyscan_t yyscanner );
+void yy_flush_buffer ( YY_BUFFER_STATE b , yyscan_t yyscanner );
+void yypush_buffer_state ( YY_BUFFER_STATE new_buffer , yyscan_t yyscanner );
+void yypop_buffer_state ( yyscan_t yyscanner );
 
-YY_BUFFER_STATE linguist_yy_scan_buffer (char *base,yy_size_t size ,yyscan_t yyscanner );
-YY_BUFFER_STATE linguist_yy_scan_string (yyconst char *yy_str ,yyscan_t yyscanner );
-YY_BUFFER_STATE linguist_yy_scan_bytes (yyconst char *bytes,yy_size_t len ,yyscan_t yyscanner );
+YY_BUFFER_STATE yy_scan_buffer ( char *base, yy_size_t size , yyscan_t yyscanner );
+YY_BUFFER_STATE yy_scan_string ( const char *yy_str , yyscan_t yyscanner );
+YY_BUFFER_STATE yy_scan_bytes ( const char *bytes, int len , yyscan_t yyscanner );
 
-void *linguist_yyalloc (yy_size_t ,yyscan_t yyscanner );
-void *linguist_yyrealloc (void *,yy_size_t ,yyscan_t yyscanner );
-void linguist_yyfree (void * ,yyscan_t yyscanner );
+void *yyalloc ( yy_size_t , yyscan_t yyscanner );
+void *yyrealloc ( void *, yy_size_t , yyscan_t yyscanner );
+void yyfree ( void * , yyscan_t yyscanner );
 
 /* Begin user sect3 */
 
@@ -236,38 +448,42 @@ void linguist_yyfree (void * ,yyscan_t yyscanner );
 
 #define YY_EXTRA_TYPE struct tokenizer_extra *
 
-int linguist_yylex_init (yyscan_t* scanner);
+int yylex_init (yyscan_t* scanner);
 
-int linguist_yylex_init_extra (YY_EXTRA_TYPE user_defined,yyscan_t* scanner);
+int yylex_init_extra ( YY_EXTRA_TYPE user_defined, yyscan_t* scanner);
 
 /* Accessor methods to globals.
    These are made visible to non-reentrant scanners for convenience. */
 
-int linguist_yylex_destroy (yyscan_t yyscanner );
+int yylex_destroy ( yyscan_t yyscanner );
 
-int linguist_yyget_debug (yyscan_t yyscanner );
+int yyget_debug ( yyscan_t yyscanner );
 
-void linguist_yyset_debug (int debug_flag ,yyscan_t yyscanner );
+void yyset_debug ( int debug_flag , yyscan_t yyscanner );
 
-YY_EXTRA_TYPE linguist_yyget_extra (yyscan_t yyscanner );
+YY_EXTRA_TYPE yyget_extra ( yyscan_t yyscanner );
 
-void linguist_yyset_extra (YY_EXTRA_TYPE user_defined ,yyscan_t yyscanner );
+void yyset_extra ( YY_EXTRA_TYPE user_defined , yyscan_t yyscanner );
 
-FILE *linguist_yyget_in (yyscan_t yyscanner );
+FILE *yyget_in ( yyscan_t yyscanner );
 
-void linguist_yyset_in  (FILE * in_str ,yyscan_t yyscanner );
+void yyset_in  ( FILE * _in_str , yyscan_t yyscanner );
 
-FILE *linguist_yyget_out (yyscan_t yyscanner );
+FILE *yyget_out ( yyscan_t yyscanner );
 
-void linguist_yyset_out  (FILE * out_str ,yyscan_t yyscanner );
+void yyset_out  ( FILE * _out_str , yyscan_t yyscanner );
 
-yy_size_t linguist_yyget_leng (yyscan_t yyscanner );
+			int yyget_leng ( yyscan_t yyscanner );
 
-char *linguist_yyget_text (yyscan_t yyscanner );
+char *yyget_text ( yyscan_t yyscanner );
 
-int linguist_yyget_lineno (yyscan_t yyscanner );
+int yyget_lineno ( yyscan_t yyscanner );
 
-void linguist_yyset_lineno (int line_number ,yyscan_t yyscanner );
+void yyset_lineno ( int _line_number , yyscan_t yyscanner );
+
+int yyget_column  ( yyscan_t yyscanner );
+
+void yyset_column ( int _column_no , yyscan_t yyscanner );
 
 /* Macros after this point can all be overridden by user definitions in
  * section 1.
@@ -275,18 +491,18 @@ void linguist_yyset_lineno (int line_number ,yyscan_t yyscanner );
 
 #ifndef YY_SKIP_YYWRAP
 #ifdef __cplusplus
-extern "C" int linguist_yywrap (yyscan_t yyscanner );
+extern "C" int yywrap ( yyscan_t yyscanner );
 #else
-extern int linguist_yywrap (yyscan_t yyscanner );
+extern int yywrap ( yyscan_t yyscanner );
 #endif
 #endif
 
 #ifndef yytext_ptr
-static void yy_flex_strncpy (char *,yyconst char *,int ,yyscan_t yyscanner);
+static void yy_flex_strncpy ( char *, const char *, int , yyscan_t yyscanner);
 #endif
 
 #ifdef YY_NEED_STRLEN
-static int yy_flex_strlen (yyconst char * ,yyscan_t yyscanner);
+static int yy_flex_strlen ( const char * , yyscan_t yyscanner);
 #endif
 
 #ifndef YY_NO_INPUT
@@ -295,7 +511,12 @@ static int yy_flex_strlen (yyconst char * ,yyscan_t yyscanner);
 
 /* Amount of stuff to slurp up with each read. */
 #ifndef YY_READ_BUF_SIZE
+#ifdef __ia64__
+/* On IA-64, the buffer size is 16k, not 8k */
+#define YY_READ_BUF_SIZE 16384
+#else
 #define YY_READ_BUF_SIZE 8192
+#endif /* __ia64__ */
 #endif
 
 /* Number of entries by which start-condition stack grows. */
@@ -309,9 +530,9 @@ static int yy_flex_strlen (yyconst char * ,yyscan_t yyscanner);
 #ifndef YY_DECL
 #define YY_DECL_IS_OURS 1
 
-extern int linguist_yylex (yyscan_t yyscanner);
+extern int yylex (yyscan_t yyscanner);
 
-#define YY_DECL int linguist_yylex (yyscan_t yyscanner)
+#define YY_DECL int yylex (yyscan_t yyscanner)
 #endif /* !YY_DECL */
 
 /* yy_get_previous_state - get the state just before the EOB char was reached */
@@ -328,9 +549,154 @@ extern int linguist_yylex (yyscan_t yyscanner);
 #undef YY_DECL
 #endif
 
+#ifndef linguist_yy_create_buffer_ALREADY_DEFINED
+#undef yy_create_buffer
+#endif
+#ifndef linguist_yy_delete_buffer_ALREADY_DEFINED
+#undef yy_delete_buffer
+#endif
+#ifndef linguist_yy_scan_buffer_ALREADY_DEFINED
+#undef yy_scan_buffer
+#endif
+#ifndef linguist_yy_scan_string_ALREADY_DEFINED
+#undef yy_scan_string
+#endif
+#ifndef linguist_yy_scan_bytes_ALREADY_DEFINED
+#undef yy_scan_bytes
+#endif
+#ifndef linguist_yy_init_buffer_ALREADY_DEFINED
+#undef yy_init_buffer
+#endif
+#ifndef linguist_yy_flush_buffer_ALREADY_DEFINED
+#undef yy_flush_buffer
+#endif
+#ifndef linguist_yy_load_buffer_state_ALREADY_DEFINED
+#undef yy_load_buffer_state
+#endif
+#ifndef linguist_yy_switch_to_buffer_ALREADY_DEFINED
+#undef yy_switch_to_buffer
+#endif
+#ifndef linguist_yypush_buffer_state_ALREADY_DEFINED
+#undef yypush_buffer_state
+#endif
+#ifndef linguist_yypop_buffer_state_ALREADY_DEFINED
+#undef yypop_buffer_state
+#endif
+#ifndef linguist_yyensure_buffer_stack_ALREADY_DEFINED
+#undef yyensure_buffer_stack
+#endif
+#ifndef linguist_yylex_ALREADY_DEFINED
+#undef yylex
+#endif
+#ifndef linguist_yyrestart_ALREADY_DEFINED
+#undef yyrestart
+#endif
+#ifndef linguist_yylex_init_ALREADY_DEFINED
+#undef yylex_init
+#endif
+#ifndef linguist_yylex_init_extra_ALREADY_DEFINED
+#undef yylex_init_extra
+#endif
+#ifndef linguist_yylex_destroy_ALREADY_DEFINED
+#undef yylex_destroy
+#endif
+#ifndef linguist_yyget_debug_ALREADY_DEFINED
+#undef yyget_debug
+#endif
+#ifndef linguist_yyset_debug_ALREADY_DEFINED
+#undef yyset_debug
+#endif
+#ifndef linguist_yyget_extra_ALREADY_DEFINED
+#undef yyget_extra
+#endif
+#ifndef linguist_yyset_extra_ALREADY_DEFINED
+#undef yyset_extra
+#endif
+#ifndef linguist_yyget_in_ALREADY_DEFINED
+#undef yyget_in
+#endif
+#ifndef linguist_yyset_in_ALREADY_DEFINED
+#undef yyset_in
+#endif
+#ifndef linguist_yyget_out_ALREADY_DEFINED
+#undef yyget_out
+#endif
+#ifndef linguist_yyset_out_ALREADY_DEFINED
+#undef yyset_out
+#endif
+#ifndef linguist_yyget_leng_ALREADY_DEFINED
+#undef yyget_leng
+#endif
+#ifndef linguist_yyget_text_ALREADY_DEFINED
+#undef yyget_text
+#endif
+#ifndef linguist_yyget_lineno_ALREADY_DEFINED
+#undef yyget_lineno
+#endif
+#ifndef linguist_yyset_lineno_ALREADY_DEFINED
+#undef yyset_lineno
+#endif
+#ifndef linguist_yyget_column_ALREADY_DEFINED
+#undef yyget_column
+#endif
+#ifndef linguist_yyset_column_ALREADY_DEFINED
+#undef yyset_column
+#endif
+#ifndef linguist_yywrap_ALREADY_DEFINED
+#undef yywrap
+#endif
+#ifndef linguist_yyget_lval_ALREADY_DEFINED
+#undef yyget_lval
+#endif
+#ifndef linguist_yyset_lval_ALREADY_DEFINED
+#undef yyset_lval
+#endif
+#ifndef linguist_yyget_lloc_ALREADY_DEFINED
+#undef yyget_lloc
+#endif
+#ifndef linguist_yyset_lloc_ALREADY_DEFINED
+#undef yyset_lloc
+#endif
+#ifndef linguist_yyalloc_ALREADY_DEFINED
+#undef yyalloc
+#endif
+#ifndef linguist_yyrealloc_ALREADY_DEFINED
+#undef yyrealloc
+#endif
+#ifndef linguist_yyfree_ALREADY_DEFINED
+#undef yyfree
+#endif
+#ifndef linguist_yytext_ALREADY_DEFINED
+#undef yytext
+#endif
+#ifndef linguist_yyleng_ALREADY_DEFINED
+#undef yyleng
+#endif
+#ifndef linguist_yyin_ALREADY_DEFINED
+#undef yyin
+#endif
+#ifndef linguist_yyout_ALREADY_DEFINED
+#undef yyout
+#endif
+#ifndef linguist_yy_flex_debug_ALREADY_DEFINED
+#undef yy_flex_debug
+#endif
+#ifndef linguist_yylineno_ALREADY_DEFINED
+#undef yylineno
+#endif
+#ifndef linguist_yytables_fload_ALREADY_DEFINED
+#undef yytables_fload
+#endif
+#ifndef linguist_yytables_destroy_ALREADY_DEFINED
+#undef yytables_destroy
+#endif
+#ifndef linguist_yyTABLES_NAME_ALREADY_DEFINED
+#undef yyTABLES_NAME
+#endif
+
 #line 118 "tokenizer.l"
 
 
-#line 335 "lex.linguist_yy.h"
+#line 701 "lex.linguist_yy.h"
 #undef linguist_yyIN_HEADER
 #endif /* linguist_yyHEADER_H */
