ac8b47a39258bda0f0429675a1582529b683ac4a
Xusinboy Bekchanov <bxusinboy@mail.ru>
2023-04-11 14:23:16 +0500

    Update test_heuristics.rb

--- test/test_heuristics.rb
+++ test/test_heuristics.rb
@@ -265,7 +265,6 @@ class TestHeuristics < Minitest::Test
 
   def test_bas_by_heuristics
     assert_heuristics({
-      "VBA" => all_fixtures("VBA", "*.bas"),
       "FreeBasic" => all_fixtures("FreeBasic", "*.bas"),
       "BASIC" => all_fixtures("BASIC", "*.bas"),
       nil => all_fixtures("VBA", "*.bas")
