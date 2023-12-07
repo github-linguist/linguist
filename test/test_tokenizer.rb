88684cd53fad72ec71505a23fc7d140d7b748b9c
Colin Seymour <colin@github.com>
2023-12-07 12:53:09 +0000

    Update test/test_tokenizer.rb
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- test/test_tokenizer.rb
+++ test/test_tokenizer.rb
@@ -99,7 +99,7 @@ class TestTokenizer < Minitest::Test
     assert_equal %w(foo COMMENT/- bar), tokenize("foo /-\nComment\n-/\nbar")
     assert_equal %w(foo COMMENT/- bar), tokenize("foo\n/-\nComment\n-/\nbar")
     # Nested comments are not processed correctly as it's rarely used and adds unnecessary complexity
-    assert_equal %w(foo COMMENT/- bar), tokenize("foo /- Comment /- Still Comment /- And Still Comment -/ bar")
+    assert_equal %w(foo COMMENT/- comment), tokenize("foo /- Comment /- Still Comment /- And Still Comment -/ comment")
     assert_equal %w(foo COMMENT/- comment3 - / bar), tokenize("foo /- comment1 /- comment2 -/ comment3 -/ bar")
     assert_equal %w(COMMENT/-), tokenize("/-\n*\n-/")
     assert_equal %w(COMMENT/-), tokenize("/-*-/")
