8c36a6ce4b49ebb0a4a6a1abbe358a65f4cf0679
Mikhail Katychev <mkatych@gmail.com>
2025-02-23 17:38:08 -0600

    Update test_language.rb
    remove unneeded assertion

--- test/test_language.rb
+++ test/test_language.rb
@@ -60,7 +60,6 @@ class TestLanguage < Minitest::Test
     assert_equal Language['Shell'], Language.find_by_alias('zsh')
     assert_equal Language['SuperCollider'], Language.find_by_alias('supercollider')
     assert_equal Language['TeX'], Language.find_by_alias('tex')
-    assert_equal Language['Tree-sitter Query'], Language.find_by_alias('tree-sitter-query')
     assert_equal Language['Tree-sitter Query'], Language.find_by_alias('tsq')
     assert_equal Language['TypeScript'], Language.find_by_alias('ts')
     assert_equal Language['Vim Script'], Language.find_by_alias('vim')
