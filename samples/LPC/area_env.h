02c2d417c84932d830fb39f6601376f9064a15de
jlchmura <jlchmura@gmail.com>
2026-01-13 14:34:26 -0500

    - tweaks to lpc patterns - add real area_env.h file

--- samples/LPC/area_env.h
+++ samples/LPC/area_env.h
@@ -1,4 +1,16 @@
+int minLevel;
+
+public int query_minLevel() {
+  return minLevel;
+}
+public void set_minLevel(int level) {
+  minLevel = level;
+}
 
 public string query_label() {
   return "Area Label";
+}
+
+public string canEnterArea() {  
+  return this_player()->query_level() < minLevel ? "You are not experienced enough to enter this area." : 0;
 }
