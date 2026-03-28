e5d934fe820c7d33aa48cd7943977643a1d0de4e
oopsio <161714242+oopsio@users.noreply.github.com>
2026-03-28 09:40:46 +0530

    Add license notice to deno.ts
    Added a software license notice to the deno.ts file.

--- samples/TypeScript/deno.ts
+++ samples/TypeScript/deno.ts
@@ -1,9 +1,17 @@
 #!/usr/bin/env -S deno run --allow-env
+
 /**
- * @file deno.ts
- * @description Demonstrates Object-Oriented Programming (OOP) in Deno using TypeScript.
- * Features encapsulated data, constructors, and methods.
+ * @author oopsio
+ * @copyright (c) 2026-present oopsio
+ * @license MIT
  */
+
+/*
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 
 /**
  * Represents a user in the system.
