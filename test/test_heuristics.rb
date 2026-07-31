0a89020311bddfd07d843811aa903aec00c8b3df
Drake53 <49623303+Drake53@users.noreply.github.com>
2026-07-31 19:11:15 +0200

    Add JASS language

--- test/test_heuristics.rb
+++ test/test_heuristics.rb
@@ -610,6 +610,14 @@ class TestHeuristics < Minitest::Test
     }, alt_name="foo.inc")
   end
 
+  def test_j_by_heuristics
+    assert_heuristics({
+      "JASS" => all_fixtures("JASS", "*.j"),
+      "Jasmin" => all_fixtures("Jasmin", "*.j"),
+      "Objective-J" => all_fixtures("Objective-J", "*.j")
+    })
+  end
+
   def test_json_by_heuristics
     assert_heuristics({
       "OASv2-json" => all_fixtures("OASv2-json", "*.json"),
